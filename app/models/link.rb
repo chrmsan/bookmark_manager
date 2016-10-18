c4eddf409f40cc0bb472ef23605e9b270bfb5efe
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-18 15:34:43 +0200

    Challenge 9 complete

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link
   property :title, String
   property :url, String
 end  
-
-  DataMapper.setup(:default, 'postgres://ubuntu:password@localhost/bookmark_manager_test')
+  #DataMapper.setup(:default, 'postgres://ubuntu:password@localhost/bookmark_manager_test')
+  DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
   DataMapper.finalize
   DataMapper.auto_upgrade!
