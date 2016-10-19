81335d0b4a7cc49a1ae7e401f031ab9f100e2bcd
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-19 17:49:22 +0100

    added multiple tag functionality and passed test

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -5,10 +5,21 @@ feature 'Adding tags' do
     visit '/links/new'
     fill_in 'url', with: 'www.facebook.com'
     fill_in 'title', with: 'Facebook'
-    fill_in 'tags', with: 'social media network'
+    fill_in 'tags', with: 'social'
     click_button 'Add Link'
 
     link = Link.get(1)
-    expect(link.tags.map(&:name)).to include('social media network')
+    expect(link.tags.map(&:name)).to include('social')
+  end
+
+  scenario 'Adding multiple tags' do
+    visit '/links/new'
+    fill_in 'url', with: 'www.facebook.com'
+    fill_in 'title', with: 'Facebook'
+    fill_in 'tags', with: 'social networking'
+    click_button 'Add Link'
+
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('social', 'networking')
   end
 end
