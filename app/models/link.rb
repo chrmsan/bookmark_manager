8ee273a29ff244a8abd204294a032ff9044d0588
B-raw <michaelbarry1989@hotmail.com>
2016-10-19 11:00:12 +0100

    Edit model

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,6 +9,8 @@ class Link
   property :url,     String
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
+configure do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
+end
