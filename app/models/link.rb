0e003ee2b2825c6f13dcb257bcf75bac3bcfe194
B-raw <michaelbarry1989@hotmail.com>
2016-10-19 09:41:21 +0100

    change database address

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,6 +9,6 @@ class Link
   property :url,     String
 end
 
-DataMapper.setup(:default, ('postgres://lnhkbfobpqejri:1TBMaDSa4bTqId9XSgHDjBw8_0@ec2-54-243-249-56.compute-1.amazonaws.com:5432/d13fotaai53k12' || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"))
+DataMapper.setup(:default, ('postgres://lnhkbfobpqejri:1TBMaDSa4bTqId9XSgHDjBw8_0@ec2-54-243-249-56.compute-1.amazonaws.com:5432/d13fotaai53k12'))
 DataMapper.finalize
 DataMapper.auto_upgrade!
