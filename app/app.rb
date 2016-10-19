aab7131575e2d68a72bb796e7bdea3ccd810abe5
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-19 20:03:26 +0200

    Almost finished stage 18

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,15 @@ require './app/models/datamapper_help.rb'
 ENV['RACK_ENV'] ||= 'development'
 
 class BookmarkManager < Sinatra::Base
+  
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
 
   get '/links' do
     @links = Link.all
@@ -27,6 +36,15 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    User.create(email: params[:email], password: params[:password])
+    redirect '/links'
   end
 
   run! if app_file == $0
