64fb4a3f6f1c6f4f399e2da253de4c4b0b4c9dbd
B-raw <michaelbarry1989@hotmail.com>
2016-10-19 10:18:11 +0100

    Change config

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative './models/link'
-require './environments'
+
 
 class BookmarkManager < Sinatra::Base
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -9,6 +9,6 @@ class Link
   property :url,     String
 end
 
-DataMapper.setup(:default, ('postgres://lnhkbfobpqejri:1TBMaDSa4bTqId9XSgHDjBw8_0@ec2-54-243-249-56.compute-1.amazonaws.com:5432/d13fotaai53k12'))
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 DataMapper.finalize
 DataMapper.auto_upgrade!
