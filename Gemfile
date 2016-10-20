93c47aefa1993e27244a77a740ad0d9c36b8f7a1
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-20 15:33:05 +0200

    stage 20 complete added flash message. all tests green

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.3.0'
+ruby '2.3.1'
 
 gem 'sinatra'
 gem 'dm-postgres-adapter'
@@ -10,6 +10,8 @@ gem 'pg'
 gem 'rake'
 gem 'rerun'
 gem 'bcrypt'
+gem 'sinatra-flash'
+
 
 group :test do
   gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'models/link'
 require_relative 'data_mapper_setup'
 require_relative 'models/user'
 
 class BookmarkManager < Sinatra::Base
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -45,15 +47,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb(:'users/new')
   end
 
   post '/users' do
-    user = User.create(email:                  params[:email],
+    @user = User.create(email:                  params[:email],
                        password:               params[:password],
                        password_confirmation:  params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/links'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/links'
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
 
