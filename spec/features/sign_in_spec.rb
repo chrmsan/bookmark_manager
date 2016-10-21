c5fb2b19c94e42b831eef8e887fdb3934784eccd
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-21 09:54:15 +0100

    Midway through exercise 21

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,10 +5,11 @@ feature 'sign in' do
     click_button 'Register'
     fill_in 'first_name', with: 'Alan'
     fill_in 'last_name', with: 'Shearer'
-    fill_in 'email', with: 'alan@nufc.com'
-    fill_in 'password', with: '1234'
+
   end
   it 'allows a new user to sign up' do
+    fill_in 'email', with: 'alan@nufc.com'
+    fill_in 'password', with: '1234'
     fill_in 'password_confirmation', with: '1234'
     expect{ click_button 'Sign up' }.to change{ User.all.count }.by(1)
     expect(page).to have_content 'Welcome, Alan'
@@ -16,9 +17,19 @@ feature 'sign in' do
   end
 
   it 'does not allow an account to be created if the specified passwords do not match' do
+    fill_in 'email', with: 'alan@nufc.com'
+    fill_in 'password', with: '1234'
     fill_in 'password_confirmation', with: '123'
     expect{ click_button 'Sign up' }.to change{ User.all.count }.by(0)
     expect(current_path).to eq '/users/authenticate'
     expect(page).to have_content 'Password and confirmation password do not match'
+  end
+
+  it 'does not allow the user to sign in without entering something in the email field' do
+    fill_in 'email', with: ''
+    fill_in 'password', with: '1234'
+    fill_in 'password', with: '1234'
+    click_button 'Sign up'
+    expect{ click_button 'Sign up' }.to change{ User.all.count }.by(0)
   end
 end
