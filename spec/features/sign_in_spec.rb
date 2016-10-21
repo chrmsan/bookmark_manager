d5d44d7fc2143b3bba3f5ad6662f71e4e1babb49
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-21 15:29:51 +0100

    1) Added failing test for signing in functionality

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,45 +5,18 @@ feature 'sign in' do
     click_button 'Register'
     fill_in 'first_name', with: 'Alan'
     fill_in 'last_name', with: 'Shearer'
-  end
-
-  it 'allows a new user to sign up' do
-    fill_in 'email', with: 'alan@nufc.com'
-    fill_in 'password', with: '1234'
-    fill_in 'password_confirmation', with: '1234'
-    expect{ click_button 'Sign up' }.to change{ User.all.count }.by(1)
-    expect(page).to have_content 'Welcome, Alan'
-    expect(User.first.email).to eq('alan@nufc.com')
-  end
-
-  it 'does not allow an account to be created if the specified passwords do not match' do
-    fill_in 'email', with: 'alan@nufc.com'
-    fill_in 'password', with: '1234'
-    fill_in 'password_confirmation', with: '123'
-    expect{ click_button 'Sign up' }.to change{ User.all.count }.by(0)
-    expect(current_path).to eq '/users/authenticate'
-    expect(page).to have_content 'Password does not match the confirmation'
-  end
-
-  it 'does not allow the user to sign in without entering something in the email field' do
-    fill_in 'email', with: nil
-    fill_in 'password', with: '1234'
-    fill_in 'password', with: '1234'
-    click_button 'Sign up'
-    expect{ click_button 'Sign up' }.to change{ User.all.count }.by(0)
-  end
-
-  it 'does not allow user to sign in again with previous registered email' do
     fill_in 'email', with: 'alan@nufc.com'
     fill_in 'password', with: '1234'
     fill_in 'password_confirmation', with: '1234'
     click_button 'Sign up'
+  end
+
+  it 'allows a registered user to sign in' do
     visit '/'
-    click_button 'Register'
+    click_button 'Sign in'
     fill_in 'email', with: 'alan@nufc.com'
     fill_in 'password', with: '1234'
-    fill_in 'password_confirmation', with: '1234'
-    expect { click_button 'Sign up' }.to_not change(User, :count)
-    expect(page).to have_content('Email is already taken')
+    expect(current_path).to eq '/links'
+    expect(page).to have_content 'Welcome, Alan'
   end
 end
